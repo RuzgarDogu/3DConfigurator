a736852aa0f317b7b46d807f4f2d0735efb47059
RuzgarDogu <doguruzgar@gmail.com>
2021-07-22 16:01:47 +0300

    v07

--- public/js/controls.js
+++ public/js/controls.js
@@ -1,42 +1,12 @@
 console.log("controls.js");
 
 const sahne = window.sahne;
-//
-$(document).on('click mouseover', '#btn-1-1', function(e) {
-  sahne.updateMaterial(0,'public/img/textures/concrete.jpg');
-})
-//
-$(document).on('click mouseover', '#btn-1-2', function(e) {
-  sahne.updateMaterial(0,'public/img/textures/pv_panel.png');
-})
-//
-$(document).on('click mouseover', '#btn-2-1', function(e) {
-  sahne.updateMaterial(2,'public/img/textures/galvanizli.jpg');
-})
 
-$(document).on('click mouseover', '#btn-2-2', function(e) {
-  sahne.updateMaterial(2,'public/img/textures/boyali.jpg');
-})
-//
-// $(document).on('click mouseover', '#btn-2-3', function(e) {
-//   sahne.updateSize(1.4);
-// })
-//
-$(document).on('click mouseover', '#btn-3-1', function(e) {
-  sahne.cloneElement();
-})
-//
-// document.addEventListener('mousemove',sahne.onDocumentMouseMove)
-// window.addEventListener('resize',sahne.resize)
-//
-//
-//Initialize tooltips
-// $('.nav-tabs > li a[title]').tooltip();
 
 var main_condition = true;
 var step_controller;
 
-function nextTab($this) {
+const nextTab = ($this) => {
   step_controller = getCurrentStep($this);
   step_controller ++;
   let nextEl = $('#ul-stepLinks').children().eq(step_controller).find('a[data-toggle="tab"]');
@@ -44,18 +14,18 @@ function nextTab($this) {
 
 }
 
-function prevTab($this) {
+const prevTab = ($this) => {
   step_controller -= 1;
   let prevEl = $('#ul-stepLinks').children().eq(step_controller).find('a[data-toggle="tab"]');
   prevEl.click();
 }
 
-function lastTab($this) {
+const lastTab = ($this) => {
   step_controller += 1;
   $('#btn-complete').click();
 }
 
-function startOver() {
+const startOver = () => {
   step_controller = 0;
   $('#a-step0').removeClass('disabled');
   $('#a-step0').click();
@@ -125,11 +95,6 @@ $.getJSON('step_managet_step_test.json',  function(json, textStatus) {})
 
   const update_userinput = (name,optid) => {
     user_input[name] = optid;
-    // let func = 'update_'+name;
-    //
-    // if (typeof sahne[func] === "function") {
-    //     sahne[func](optid);
-    // }
     sahne.updateSahne(name,optid);
   }
 
